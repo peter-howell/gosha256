2876cc5bf9fffcbe2e10d09d601192a4f88a6c52
peter-howell <44275653+peter-howell@users.noreply.github.com>
2025-11-24 17:47:54 -0500

    processes in chunks, and takes file name(s) as arguments

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"math/bits"
 	"os"
 )
@@ -18,27 +19,22 @@ func rightrotate(x uint32, k int) uint32 {
 }
 
 type Hasher struct {
-	h [8]uint32
-	words [64]uint32
+	h [8]uint32 // hash values
+	words [64]uint32 // storage for the words
+	nBitsRead uint64 // number of bits read so far
+	done bool
 }
 
 func NewHasher() *Hasher {
 	return &Hasher{
 		h: [8]uint32{0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19},
 		words: [64]uint32{},
+		nBitsRead: 0,
+		done: false,
 	}
 }
 
-var h0 uint32 = 0x6a09e667
-var h1 uint32 = 0xbb67ae85
-var h2 uint32 = 0x3c6ef372
-var h3 uint32 = 0xa54ff53a
-var h4 uint32 = 0x510e527f
-var h5 uint32 = 0x9b05688c
-var h6 uint32 = 0x1f83d9ab
-var h7 uint32 = 0x5be0cd19
-
-var k []uint32 = []uint32{
+var k [64]uint32 = [64]uint32{
 	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
 	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
 	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -55,36 +51,36 @@ var k []uint32 = []uint32{
 // partial chunk (less than 4 bytes) is ignored.
 func getWords(data *[64]byte, words *[64]uint32) {
 	for i := range 16 {
-		words[i] = binary.BigEndian.Uint32(data[4*i : 4*i+4])
+		(*words)[i] = binary.BigEndian.Uint32((*data)[4*i : 4*i + 4])
 	}
 }
 
 
-func processChunk(chunk *[64]byte, words *[64]uint32) error {
+func (hasher *Hasher) processChunk(chunk *[64]byte) {
 
 	// convert each 4 bytes into a uint32. Store them in here.
-	getWords(chunk, words)
+	getWords(chunk, &hasher.words)
 
-	a := h0
-	b := h1
-	c := h2
-	d := h3
-	e := h4
-	f := h5
-	g := h6
-	h := h7
+	a := hasher.h[0]
+	b := hasher.h[1]
+	c := hasher.h[2]
+	d := hasher.h[3]
+	e := hasher.h[4]
+	f := hasher.h[5]
+	g := hasher.h[6]
+	h := hasher.h[7]
 
 	//extend the first 16 words into the remaining 48 words
 	for i := 16; i < 64; i++ {
-		s0 := rightrotate(words[i-15], 7) ^ rightrotate(words[i-15], 18) ^ (words[i-15] >> 3)
-		s1 := rightrotate(words[i-2], 17) ^ rightrotate(words[i-2], 19) ^ (words[i-2] >> 10)
-		words[i] = words[i-16] + s0 + words[i-7] + s1
+		s0 := rightrotate(hasher.words[i-15], 7) ^ rightrotate(hasher.words[i-15], 18) ^ (hasher.words[i-15] >> 3)
+		s1 := rightrotate(hasher.words[i-2], 17) ^ rightrotate(hasher.words[i-2], 19) ^ (hasher.words[i-2] >> 10)
+		hasher.words[i] = hasher.words[i-16] + s0 + hasher.words[i-7] + s1
 	}
 	// compression function main loop:
 	for i := range 64 {
 		S1 := rightrotate(e, 6) ^ rightrotate(e, 11) ^ rightrotate(e, 25)
 		ch := (e & f) ^ (^e & g)
-		temp1 := h + S1 + ch + k[i] + words[i]
+		temp1 := h + S1 + ch + k[i] + hasher.words[i]
 		S0 := rightrotate(a, 2) ^ rightrotate(a, 13) ^ rightrotate(a, 22)
 		maj := (a & b) ^ (a & c) ^ (b & c)
 		temp2 := S0 + maj
@@ -99,62 +95,61 @@ func processChunk(chunk *[64]byte, words *[64]uint32) error {
 		a = temp1 + temp2
 	}
 	// add the compressed chunk to the current hash value
-	h0 = h0 + a
-	h1 = h1 + b
-	h2 = h2 + c
-	h3 = h3 + d
-	h4 = h4 + e
-	h5 = h5 + f
-	h6 = h6 + g
-	h7 = h7 + h
-
-	return nil
+	hasher.h[0] = hasher.h[0] + a
+	hasher.h[1] = hasher.h[1] + b
+	hasher.h[2] = hasher.h[2] + c
+	hasher.h[3] = hasher.h[3] + d
+	hasher.h[4] = hasher.h[4] + e
+	hasher.h[5] = hasher.h[5] + f
+	hasher.h[6] = hasher.h[6] + g
+	hasher.h[7] = hasher.h[7] + h
 
 }
 
 // SHA256Sum computes the SHA-256 hash of a byte array.
-func SHA256Sum(msg []byte) [32]byte {
-	ogBitLen := uint64(len(msg) * 8)         // original message length in bits
-	neededLenBits := ogBitLen + 1            // add the following 1 bit
-	K := (448 - (neededLenBits % 512)) % 512 // add 0 bits until len %512 = 448
-	neededLenBits += K + 64                  // with the uint64 of the length.
+func SHA256Sum(conn io.Reader) *[32]byte {
+	hasher := NewHasher()
 
-	message := make([]byte, neededLenBits/8) // message as an array of bytes, padded correctly
-	copy(message, msg)
-
-	message[len(msg)] = 0x80
-	binary.BigEndian.PutUint64(message[(neededLenBits/8)-8:], ogBitLen) // length of original message at the end
-
-	buf := bytes.NewBuffer(message)
-
-	words := [64]uint32{}
-	chunk := [64]byte{}
+	chunkBuf := [64]byte{}
 
 	for {
 		// chunk of 512 bits (64 bytes)
-		// chunk := buf.Next(64)
-		n, err := buf.Read(chunk[:])
-		fmt.Printf("Read %d bytes\n", n)
-		if n == 0 {
+		nRead, err := conn.Read(chunkBuf[:])
+		if nRead == 0 && hasher.done {
 			break
 		}
-		if err != nil {
+		if err != nil  && err != io.EOF {
 			fmt.Printf("Got some error, %v", err)
 			break
 		}
-		processChunk(&chunk, &words)
+		hasher.nBitsRead += uint64(nRead*8)
+		if nRead != 64 {
+			// now need to pad. will need to add at least 9 bytes (uint64 of length of original message in bits + the 1 bit appended to message)
+			// if not enough space for 9 bytes to be added, then we'll need to do another pass
+			chunkBuf[nRead] = 0x80
+			for i := nRead+1; i < 64; i++ {
+				chunkBuf[i] = 0
+			}
+			if nRead > 64 - 9 {
+				hasher.processChunk(&chunkBuf)
+				chunkBuf = [64]byte{}
+			}
+			binary.BigEndian.PutUint64(chunkBuf[64 - 8:], hasher.nBitsRead)
+			hasher.done = true
+		}
+		hasher.processChunk(&chunkBuf)
 	}
 
 	result := [32]byte{}
-	binary.BigEndian.PutUint32(result[0:], h0)
-	binary.BigEndian.PutUint32(result[4:], h1)
-	binary.BigEndian.PutUint32(result[8:], h2)
-	binary.BigEndian.PutUint32(result[12:], h3)
-	binary.BigEndian.PutUint32(result[16:], h4)
-	binary.BigEndian.PutUint32(result[20:], h5)
-	binary.BigEndian.PutUint32(result[24:], h6)
-	binary.BigEndian.PutUint32(result[28:], h7)
-	return result
+	binary.BigEndian.PutUint32(result[0:], hasher.h[0])
+	binary.BigEndian.PutUint32(result[4:], hasher.h[1])
+	binary.BigEndian.PutUint32(result[8:], hasher.h[2])
+	binary.BigEndian.PutUint32(result[12:], hasher.h[3])
+	binary.BigEndian.PutUint32(result[16:], hasher.h[4])
+	binary.BigEndian.PutUint32(result[20:], hasher.h[5])
+	binary.BigEndian.PutUint32(result[24:], hasher.h[6])
+	binary.BigEndian.PutUint32(result[28:], hasher.h[7])
+	return &result
 }
 
 // main computes and prints the SHA-256 digest of the first
@@ -162,16 +157,32 @@ func SHA256Sum(msg []byte) [32]byte {
 // the message in 512-bit chunks using the SHA-256 compression
 // function, and prints the 32-byte digest in hexadecimal.
 func main() {
+	var conn io.Reader
+	var err error
 	if len(os.Args) < 2 {
-		fmt.Println("didn't receive an argument. there should be an argument here.")
-		return
+		fmt.Println("didn't receive an argument, using stdin")
+		conn = bufio.NewReader(os.Stdin)
+		result := SHA256Sum(conn)
+
+		// Pad the message to proper length
+		fmt.Printf("%x  -\n", *result)
+	} else {
+		// take the message from a cmd line argument and store it as a byte array
+		for i := 1; i < len(os.Args); i++ {
+			fname := os.Args[i]
+			conn, err = os.Open(fname)
+
+			if err != nil {
+				fmt.Printf("Got an error trying to open %s, %v\n", fname, err)
+				continue
+			}
+			result := SHA256Sum(conn)
+
+			// Pad the message to proper length
+			fmt.Printf("%x  %s\n", *result, fname)
+		}
 	}
-	// take the message from a cmd line argument and store it as a byte array
-	msg := []byte(os.Args[1])
 
-	result := SHA256Sum(msg)
 
-	// Pad the message to proper length
-	fmt.Printf("Result:\n%x\n", result)
 }
 
